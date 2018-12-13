c6c925414c64bf42b289e893acd8d8e3b30cd30b
Fred <sohnf90@yahoo.com>
2018-12-13 11:17:51 -0800

    Updates

---
+++ precache-manifest.a63f4a0b122cccc3fa7560cb1e61c282.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7d533ab09956ceb10fb",
+    "url": "/Conways-Life/static/js/runtime~main.a7d533ab.js"
+  },
+  {
+    "revision": "0a91302ff8c7e83be978",
+    "url": "/Conways-Life/static/js/main.0a91302f.chunk.js"
+  },
+  {
+    "revision": "de556fe8e320c45bde5b",
+    "url": "/Conways-Life/static/js/1.de556fe8.chunk.js"
+  },
+  {
+    "revision": "0a91302ff8c7e83be978",
+    "url": "/Conways-Life/static/css/main.c1b13099.chunk.css"
+  },
+  {
+    "revision": "03b99ec28217c6e428a00746e80c021f",
+    "url": "/Conways-Life/index.html"
+  }
+];
